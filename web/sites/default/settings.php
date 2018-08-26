8bc370e0fb25663561630df7a49ee81f3dbc044e
Sean Dietrich <sean@kanopistudios.com>
2018-08-26 08:45:39 -0700

    Update settings.php

--- web/sites/default/settings.php
+++ web/sites/default/settings.php
@@ -69,7 +69,7 @@ if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && ($_SERVER['HTTPS'] === 'OFF') && 
 if (isset($_SERVER['PANTHEON_ENVIRONMENT']) && ($_SERVER['PANTHEON_ENVIRONMENT'] === 'live') && (php_sapi_name() != "cli")) {
   if ($_SERVER['HTTP_HOST'] != '2018.badcamp.net' || !isset($_SERVER['HTTP_X_SSL']) || $_SERVER['HTTP_X_SSL'] != 'ON' ) {
     header('HTTP/1.0 301 Moved Permanently');
-    header('Location: https://2018.badcamp.net'. $_SERVER['REQUEST_URI']);
+    header('Location: https://2018.badcamp.org'. $_SERVER['REQUEST_URI']);
     exit();
   }
 }
